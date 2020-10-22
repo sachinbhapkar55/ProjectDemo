2dd31be0260903c65c634aaf4227ea4ab803bba9
sachinbhapkar55 <70762458+sachinbhapkar55@users.noreply.github.com>
2020-10-22 18:15:44 +0530

    Update Controller.java
    Git Changed -m1

--- GitDemo/src/com/cjc/Controller.java
+++ GitDemo/src/com/cjc/Controller.java
@@ -2,6 +2,13 @@ package com.cjc;
 
 public class Controller {
 
+	public void m1()
+	{
+		System.out.println("git m1");
+
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
